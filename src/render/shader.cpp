525e4ddc2f7c73558c8637f266de423d19cede83
tk w <tkgamegroup@outlook.com>
2017-09-29 11:50:02 +0800

    pre compiled shaders

--- src/render/shader.cpp
+++ src/render/shader.cpp
@@ -63,8 +63,9 @@ namespace tke
 			}
 		}
 
+		OnceFileBuffer spvFile(spvFilename);
+
 		{
-			OnceFileBuffer spvFile(spvFilename);
 			VkShaderModuleCreateInfo shaderModuleCreateInfo = {};
 			shaderModuleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
 			shaderModuleCreateInfo.codeSize = spvFile.length;
@@ -76,50 +77,118 @@ namespace tke
 			device.mtx.unlock();
 		}
 
-		// do reflection
+		auto resFilename = spvFilename + ".res";
+		if (std::experimental::filesystem::exists(resFilename) && 
+			std::experimental::filesystem::last_write_time(resFilename) > std::experimental::filesystem::last_write_time(spvFilename))
 		{
-			std::ifstream spv_file(spvFilename, std::ios::binary);
-			auto size = tke::file_length(spv_file);
-			std::vector<unsigned int> spv_data(size / sizeof(unsigned int));
-			spv_file.read((char*)spv_data.data(), size);
+			std::ifstream resFile(resFilename, std::ios::binary);
 
-			spirv_cross::CompilerGLSL glsl(std::move(spv_data));
+			int uboCount;
+			resFile & uboCount;
+			for (int i = 0; i < uboCount; i++)
+			{
+				int set;
+				resFile & set;
+				if (set >= descriptors.size())
+					descriptors.resize(set + 1);
+
+				Descriptor d;
+				d.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+				resFile > d.name;
+				resFile & d.binding;
+				resFile & d.count;
+				descriptors[set].push_back(d);
+			}
+			int imageCount;
+			resFile & imageCount;
+			for (int i = 0; i < imageCount; i++)
+			{
+				int set;
+				resFile & set;
+				if (set >= descriptors.size())
+					descriptors.resize(set + 1);
+
+				Descriptor d;
+				d.type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+				resFile > d.name;
+				resFile & d.binding;
+				resFile & d.count;
+				descriptors[set].push_back(d);
+			}
+			int pcSize;
+			resFile & pcSize;
+			if (pcSize > 0)
+			{
+				PushConstantRange p;
+				p.offset = 0; // 0 always
+				p.size = pcSize;
+				pushConstantRanges.push_back(p);
+			}
+		}
+		else
+		{
+			// do reflection
+			std::vector<unsigned int> spv_vec(spvFile.length / sizeof(unsigned int));
+			memcpy(spv_vec.data(), spvFile.data, spvFile.length);
+
+			spirv_cross::CompilerGLSL glsl(std::move(spv_vec));
 
 			spirv_cross::ShaderResources resources = glsl.get_shader_resources();
 
+			std::ofstream resFile(resFilename, std::ios::binary);
+
+			int uboCount = resources.uniform_buffers.size();
+			resFile & uboCount;
 			for (auto &r : resources.uniform_buffers)
 			{
-				auto set = glsl.get_decoration(r.id, spv::DecorationDescriptorSet);
+				int set = glsl.get_decoration(r.id, spv::DecorationDescriptorSet);
 				if (set >= descriptors.size())
 					descriptors.resize(set + 1);
+				resFile & set;
 
 				Descriptor d;
+				d.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 				d.name = r.name;
 				d.binding = glsl.get_decoration(r.id, spv::DecorationBinding);
-				d.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 				d.count = glsl.get_type(r.type_id).array[0];
 				descriptors[set].push_back(d);
+
+				resFile < d.name;
+				resFile & d.binding;
+				resFile & d.count;
 			}
+			int imageCount = resources.sampled_images.size();
+			resFile & imageCount;
 			for (auto &r : resources.sampled_images)
 			{
-				auto set = glsl.get_decoration(r.id, spv::DecorationDescriptorSet);
+				int set = glsl.get_decoration(r.id, spv::DecorationDescriptorSet);
 				if (set >= descriptors.size())
 					descriptors.resize(set + 1);
+				resFile & set;
 
 				Descriptor d;
+				d.type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
 				d.name = r.name;
 				d.binding = glsl.get_decoration(r.id, spv::DecorationBinding);
-				d.type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
 				d.count = glsl.get_type(r.type_id).array[0];
 				descriptors[set].push_back(d);
+
+				resFile < d.name;
+				resFile & d.binding;
+				resFile & d.count;
 			}
+
+			int pcSize = 0;
 			for (auto &r : resources.push_constant_buffers)
 			{
-				tke::PushConstantRange p;
+				PushConstantRange p;
 				p.offset = 0; // 0 always
 				p.size = glsl.get_declared_struct_size(glsl.get_type(r.type_id));
 				pushConstantRanges.push_back(p);
+
+				pcSize = p.size;
 			}
+			resFile & pcSize;
 		}
 	}
 
