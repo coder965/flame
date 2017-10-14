83b615ef01693a78d70de8c214e7530767c945bf
tk <594959742@qq.com>
2017-10-15 04:38:45 +0800

    1

--- src/utils.h
+++ src/utils.h
@@ -52,7 +52,7 @@ template<size_t s> struct Sizer {};
 
 // hash
 
-constexpr unsigned int _HASH(char const * str, unsigned int seed)
+inline constexpr unsigned int _HASH(char const * str, unsigned int seed)
 {
 	return 0 == *str ? seed : _HASH(str + 1, seed ^ (*str + 0x9e3779b9 + (seed << 6) + (seed >> 2)));
 }
