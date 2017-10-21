6e79abcade0717bfb01b547b5619f3d7f0b379c6
tkgamegroup <594959742@qq.com>
2017-10-21 21:03:02 +0800

    emmmm

--- editor/debug.cpp
+++ editor/debug.cpp
@@ -7,7 +7,7 @@ void DebugWidget::show()
 {
 	ImGui::Begin("Debug##widget", &opened);
 
-	ImGui::Image((ImTextureID)tke::debugImages[0].second->index, ImVec2(tke::debugImages[0].second->levels[0].cx, tke::debugImages[0].second->levels[0].cx));
+	//ImGui::Image((ImTextureID)tke::debugImages[0].second->index, ImVec2(tke::debugImages[0].second->levels[0].cx, tke::debugImages[0].second->levels[0].cx));
 
 	ImGui::End();
 }
