dd1fe5bf53ba4dc244c88d33485ca0f0af029243
tk w <tkgamegroup@outlook.com>
2018-03-05 09:25:27 +0800

    android

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,14 +1,35 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-#include <android/native_activity.h>
+#include <android_native_app_glue.h>
 #include <dlfcn.h>
 
-void ANativeActivity_onCreate(ANativeActivity* activity,
-                              void* savedState, size_t savedStateSize) {
-    auto libvulkan = dlopen("libvulkan.so", RTLD_NOW | RTLD_LOCAL);
-    if (!libvulkan)
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
+
+extern "C" {
+void android_main(android_app *app) {
+    auto lib_vulkan = dlopen("libvulkan.so", RTLD_NOW | RTLD_LOCAL);
+    if (!lib_vulkan)
         return;
-    int a = 1;
-    while(true);
+
+    int frames = 0;
+
+    while(true)
+    {
+        int ident;
+        int events;
+        android_poll_source *source;
+        while ((ident = ALooper_pollAll(0, nullptr, &events, (void**)&source)) >= 0)
+        {
+            if (source != nullptr)
+                source->process(app, source);
+
+            if (app->destroyRequested)
+                return;
+        }
+
+        LOGI("%d", frames);
+        frames++;
+    }
+}
 }
