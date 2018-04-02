af5627e75f31422423206bccca0a4f958fcdf353
tk w <tkgamegroup@outlook.com>
2018-04-02 21:31:41 +0800

    2

--- src/flame/image.h
+++ src/flame/image.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <flame/exports.h>
-
 #include <vector>
 #include <string>
 #include <memory>
--- tools/tk_test/src/main.cpp
+++ tools/tk_test/src/main.cpp
@@ -1,16 +1,5 @@
-#include <vector>
-#include <string>
-
-static const char *sdf_text_chars = " 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-static int sdf_text_char_count = strlen(sdf_text_chars);
-static int sdf_text_size = 32;
-
 int main(int argc, char **args)
 {
-	std::string cl(sdf_text_chars);
-	cl += " ";
-	cl += std::to_string(sdf_text_size);
-	//exec("sdf_generator", cl.c_str());
 
 	return 0;
 }
